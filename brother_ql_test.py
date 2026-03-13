65fd43a77d64bf51a30d5dc44fa7295a168af3ab
Tillorly <tillo.bosshart@gmail.com>
2026-03-13 21:02:59 +0100

    Line endings to LF

--- brother_ql_test.py
+++ brother_ql_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from label import StikkaLabel
 from printer_ql import BrotherPrintJob, find_brother_ql
 import time
@@ -7,11 +9,11 @@ printers = find_brother_ql("pyusb")
 time.sleep(2)
 
 
-for serial, printer in printers.items():
-    for i in range(5):
-        label = StikkaLabel(50, 10)
-        label.add_text(f"Hello, World! {i}", x=0, y=10, char_height=4, char_width=4, line_width=50, justification='L', font='arial.ttf')
-        l = label.render_image()
-        print_job= BrotherPrintJob(l)
-        printer.add_to_queue(print_job)
-    printer.handle_queue()
+# for serial, printer in printers.items():
+#     for i in range(5):
+#         label = StikkaLabel(50, 10)
+#         label.add_text(f"Hello, World! {i}", x=0, y=10, char_height=4, char_width=4, line_width=50, justification='L', font='arial.ttf')
+#         l = label.render_image()
+#         print_job= BrotherPrintJob(l)
+#         printer.add_to_queue(print_job)
+#     printer.handle_queue()
