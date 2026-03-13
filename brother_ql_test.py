2d870024953b980c0ff63c0dd9969cec01c14e5a
Tillorly <tillo.bosshart@gmail.com>
2026-03-13 20:46:08 +0100

    habemus queue

--- brother_ql_test.py
+++ brother_ql_test.py
@@ -7,8 +7,11 @@ printers = find_brother_ql("pyusb")
 time.sleep(2)
 
 
-    for serial, printer in printers.items():
-        label = StikkaLabel(50, 20)
-        label.add_text("Hello, World!", x=0, y=10, char_height=4, char_width=4, line_width=50, justification='L', font='arial.ttf')
-        label.add_text("Hello, World!", x=10, y=30, char_height=10, char_width=4, line_width=50, justification='C', font='arial.ttf')
-        printer.print_image(label.render_image())
+for serial, printer in printers.items():
+    for i in range(5):
+        label = StikkaLabel(50, 10)
+        label.add_text(f"Hello, World! {i}", x=0, y=10, char_height=4, char_width=4, line_width=50, justification='L', font='arial.ttf')
+        l = label.render_image()
+        print_job= BrotherPrintJob(l)
+        printer.add_to_queue(print_job)
+    printer.handle_queue()
