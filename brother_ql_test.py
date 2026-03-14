e4aad3d7e55d026837c3568291a3f7bd9f5377d8
Tillorly <tillo.bosshart@gmail.com>
2026-03-14 06:41:50 +0100

    Test upsated

--- brother_ql_test.py
+++ brother_ql_test.py
@@ -9,11 +9,14 @@ printers = find_brother_ql("pyusb")
 time.sleep(2)
 
 
-# for serial, printer in printers.items():
-#     for i in range(5):
-#         label = StikkaLabel(50, 10)
-#         label.add_text(f"Hello, World! {i}", x=0, y=10, char_height=4, char_width=4, line_width=50, justification='L', font='arial.ttf')
-#         l = label.render_image()
-#         print_job= BrotherPrintJob(l)
-#         printer.add_to_queue(print_job)
-#     printer.handle_queue()
+for serial, printer in printers.items():
+    for i in range(1):
+        label = StikkaLabel(printer.status.media_width, 15)
+        label.add_text("L x0,y0,h3", x=0, y=0, char_height=3, char_width=1.0, line_width=printer.status.media_width, justification='L', font='/usr/share/fonts/TTF/HeavyDataNerdFontPropo-Regular.ttf') 
+        label.add_text("C x0,y7,h3", x=0, y=7, char_height=3, char_width=1.0, line_width=printer.status.media_width, justification='C', font='/usr/share/fonts/OTF/OpenDyslexicNerdFontPropo-Regular.otf') 
+        label.add_text("R x0,y1,h5", x=0, y=1, char_height=5, char_width=0.5, line_width=printer.status.media_width, justification='R', font='/usr/share/fonts/Adwaita/AdwaitaMono-Bold.ttf') 
+
+        l = label.render_image(framing=True)
+        print_job= BrotherPrintJob(l)
+        printer.add_to_queue(print_job)
+    printer.handle_queue()
