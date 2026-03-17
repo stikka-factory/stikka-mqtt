cbb355c794effb6cf3f676d9249232d5ce1a9701
Tillorly <tillo.bosshart@gmail.com>
2026-03-17 11:30:46 +0100

    improved logger config

--- logger.py
+++ logger.py
@@ -1,10 +1,24 @@
 import logging
+import PIL
+from rich.highlighter import JSONHighlighter, NullHighlighter, RegexHighlighter
 from rich.logging import RichHandler
 from rich import print
+from PIL import PngImagePlugin
 
 FORMAT = "%(message)s"
 logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+    level="INFO", 
+    format=FORMAT, 
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(
+            rich_tracebacks=True, 
+            tracebacks_suppress=[PIL],
+            markup=True,
+            highlighter=NullHighlighter()
+        )    
+    ]
 )
+
 
 log = logging.getLogger("rich")
