e87782301f5dbaf964ba66338c98a6e9846766c5
tillorly <tillo.bosshart@gmail.com>
2026-05-13 17:09:05 +0200

    install process for raspi streamlined

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/sh
 
+# This script will install stikka-NG on a  raspberry pi with the username pi. 
+# It will install the necessary dependencies, 
+# copy the configuration files, 
+# and set up the systemd service.
+
+
 echo "installing uv" 
 wget -qO- https://astral.sh/uv/install.sh | sh
+
+echo "installing pythion dependencies"
+uv sync
+
+echo "installing npm"
+sudo apt update
+sudo apt install npm -y
 
 echo "copy settings"
 cp default_config.json config.json
@@ -13,3 +26,6 @@ sudo udevadm control --reload-rules && sudo udevadm trigger
 
 echo "copy service"
 sudo cp stikka-NG.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable stikka-NG.service
+sudo systemctl start stikka-NG.service
