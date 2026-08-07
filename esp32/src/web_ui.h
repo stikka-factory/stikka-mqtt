1b0cfe09bdc718dbc2bc9ea06bf9051d630adb1a
Tillo <tillo.bosshart@gmail.com>
2026-08-07 12:30:42 +0200

    Moved ESP FW to an own repo

--- esp32/src/web_ui.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// Registers routes (config page, logs page, captive-portal redirects) and
-// starts the HTTP server. Call once from setup(), after wifiManagerSetup().
-void webUiSetup();
-
-// Services pending HTTP requests. Call once per loop() iteration.
-void webUiLoop();
